0e1358c638b6749268f3b5a4ac01d6e602206259
Eli Belash <elibelash@gmail.com>
2019-09-12 21:48:59 +0300

    Base Commit from SciSharp/CodeMinion

---
+++ src/Regen.Core/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using Regen.Compiler;
+
+namespace Regen {
+    public static class Program {
+        public static void Main() {
+            var code = File.ReadAllText("testfile.cs");
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
